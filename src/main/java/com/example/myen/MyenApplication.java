89a9cf4cdcdb49b992f98efeaf644c732b7a8c4b
iamyinweijianGH <33975773+iamyinweijianGH@users.noreply.github.com>
2021-10-13 11:14:02 +0800

    Update MyenApplication.java

--- src/main/java/com/example/myen/MyenApplication.java
+++ src/main/java/com/example/myen/MyenApplication.java
@@ -1,5 +1,6 @@
 package com.example.myen;
 
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,11 +11,15 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -38,6 +43,25 @@ public class MyenApplication implements CommandLineRunner {
         Collections.shuffle(strings);
         model.addAttribute("list", strings);
         return "index";
+    }
+
+    @GetMapping("/r")
+    public Object r(Model model) {
+        Collection<List<String>> values = F_MAP.values();
+        List<String> strings = values.stream().flatMap(List::stream).collect(Collectors.toList());
+        Collections.shuffle(strings);
+        model.addAttribute("list", strings);
+        return "index";
+    }
+
+    @PostMapping("/ok")
+    @ResponseBody
+    public void r(@RequestBody JSONObject jsonObject) {
+        String key = jsonObject.getString("key");
+        String k = key.substring(0, 1);
+        List<String> strings = F_MAP.get(k);
+        strings.remove(key);
+        F_MAP.put(k, strings);
     }
 
     public static void main(String[] args) {
