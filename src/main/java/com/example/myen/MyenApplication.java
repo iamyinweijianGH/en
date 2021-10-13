55d7e950cfd361d465888a597331ff242fa2a4e8
iamyinweijianGH <33975773+iamyinweijianGH@users.noreply.github.com>
2021-10-13 15:47:08 +0800

    Update MyenApplication.java

--- src/main/java/com/example/myen/MyenApplication.java
+++ src/main/java/com/example/myen/MyenApplication.java
@@ -22,9 +22,11 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -33,6 +35,7 @@ import java.util.stream.Collectors;
 public class MyenApplication implements CommandLineRunner {
 
     public final Map<String, List<String>> F_MAP = new HashMap<>();
+    public final Set<String> F_SET = new HashSet<>();
 
     @Autowired
     ResourceLoader resourceLoader;
@@ -54,11 +57,20 @@ public class MyenApplication implements CommandLineRunner {
         return "index";
     }
 
+    @GetMapping("/rr")
+    public Object rr(Model model) {
+        List<String> strings = new ArrayList<>(F_SET);
+        Collections.shuffle(strings);
+        model.addAttribute("list", strings);
+        return "index";
+    }
+
     @PostMapping("/ok")
     @ResponseBody
     public void r(@RequestBody JSONObject jsonObject) {
         String key = jsonObject.getString("key");
         String k = key.substring(0, 1);
+        F_SET.add(k);
         List<String> strings = F_MAP.get(k);
         strings.remove(key);
         F_MAP.put(k, strings);
